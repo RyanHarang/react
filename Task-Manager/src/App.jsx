e9f432206526193c19ad3a11017788ec9398e45a
Ryan <harangr@wwu.edu>
2023-05-07 11:54:10 -0700

    Made a toggling button

--- Task-Manager/src/App.jsx
+++ Task-Manager/src/App.jsx
@@ -3,9 +3,9 @@ import Counter from "./components/Counter";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-/*
+
 import "bootstrap/dist/css/bootstrap.css";
-*/
+
 function App() {
   return (
     <>
--- Task-Manager/src/components/Counter.jsx
+++ Task-Manager/src/components/Counter.jsx
@@ -2,19 +2,38 @@ import React from "react";
 import { useState } from "react";
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  const [done, setDone] = useState(false);
+  const [status, setStatus] = useState("Incomplete");
 
   function handleClick() {
-    setCount(count + 1);
+    setDone(!done);
+    updateStatus();
+  }
+
+  function handleClear() {
+    setDone(false);
+    updateStatus();
+  }
+
+  function updateStatus() {
+    {
+      status == "Incomplete" ? setStatus("Complete") : setStatus("Incomplete");
+    }
   }
 
   return (
-    <button
-      className={count === 0 ? "btn btn-secondary" : "btn btn-primary"}
-      onClick={handleClick}
-    >
-      Count = {count}
-    </button>
+    <>
+      <button
+        className={!done ? "btn btn-secondary" : "btn btn-primary"}
+        onClick={handleClick}
+      >
+        This taks is {status}
+      </button>
+      <button className="btn btn-secondary clear" onClick={handleClear}>
+        Clear
+      </button>
+      <br />
+    </>
   );
 }
 
